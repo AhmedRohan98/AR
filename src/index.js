abaad98801b290dfdd5d454dca3b0a40ee82ace0
rohan <rohan@gmail.com>
2024-08-07 12:57:10 +0500

    Initial Push

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import DemoVTO from './js/demos/VTO.js';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import './styles/index.scss';
+
+render(
+  <AppContainer>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <DemoVTO />
+        </Route>
+      </Switch>
+    </Router>
+  </AppContainer>,
+  document.querySelector('#root')
+);
